16182391141ed971ddbfdf1deda611cbf26000ad
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2024-02-10 00:54:32 +0530

    update

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def run_model(file: UploadFile = Form(...)):
     generated_ids = model.generate(pixel_values)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-    return {"generated_text": generated_text}
+    return generated_text
 
 
 
